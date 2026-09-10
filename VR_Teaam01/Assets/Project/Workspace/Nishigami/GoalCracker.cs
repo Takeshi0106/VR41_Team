b4f97cd3208c5021da41320533ba4abcebb9c1ed
MatsudaT <matsudat.30692@gmail.com>
2026-09-10 18:47:23 +0900

    ゴールエフェクトに終了処理がなかったため追加

--- VR_Teaam01/Assets/Project/Workspace/Nishigami/GoalCracker.cs
+++ VR_Teaam01/Assets/Project/Workspace/Nishigami/GoalCracker.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public class GoalCracker : MonoBehaviour
 {
     [SerializeField] private ParticleSystem leftCracker;
     [SerializeField] private ParticleSystem rightCracker;
+    [Header("パーティクルの再生時間")]
+    [SerializeField] private float m_EffectTime = 2.0f;
 
-    public void PlayGoalEffect()
+    public async void PlayGoalEffect()
     {
-
         leftCracker.Play();
         rightCracker.Play();
+
+        await UniTask.Delay(System.TimeSpan.FromSeconds(m_EffectTime));
+
+        leftCracker.Stop();
+        rightCracker.Stop();
     }
 }
